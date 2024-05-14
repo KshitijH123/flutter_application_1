bb403812c69aa9e7be126dfa07a4007f5c6cfaa1
Kshitij <kshitijhapase@gmail.com>
2024-05-14 20:25:38 +0530

    commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,8 +8,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final textStyle = const TextStyle(fontWeight: FontWeight.bold, fontSize: 20);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber.shade400,
+        title: const Text('My First Application'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Row(
+              children: [
+                const Icon(Icons.arrow_circle_right_sharp,color: Colors.red,),
+                const SizedBox(width: 16),
+                Text(
+                  'Name : ',
+                  style: textStyle,
+                ),
+                Image.network(
+                  'https://upload.wikimedia.org/wikipedia/commons/0/0e/Lady_Gaga_at_Joe_Biden%27s_inauguration_%28cropped_5%29.jpg',
+                  height: 200,
+                  width: 200,
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'RollNo : ',
+              style: textStyle,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Address : ',
+              style: textStyle,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:flutter_application_1/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
